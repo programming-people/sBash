73c17d982fa59955934b64b935123f8240d1b890
mewmer <unitia.uni@gmail.com>
2024-09-15 02:46:37 +0900

    fix some

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config, make_url
 from sqlalchemy import pool
 
 from alembic import context
-
+from models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -17,7 +17,6 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from models import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
--- backend/crud.py
+++ backend/crud.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import Session
 from fastapi import Depends
 from DB import db
 
-def read_users(db: Session = Depends(db.get_session)):
+def read_users(db: Session):
     users = db.query(User).all()
     return users
