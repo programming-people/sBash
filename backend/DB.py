a56e091bac1622d5241c24193aefe0d575261686
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-09-16 15:47:23 +0900

    データベースの設定

--- backend/DB.py
+++ backend/DB.py
@@ -1,13 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-DATABASE_URL = "postgresql://username:password@localhost/mydatabase"
+HOST = os.getenv("HOST")
+USER_NAME = os.getenv("USER_NAME")
+DB_NAME = os.getenv("DB_NAME")
+PASSWORD = os.getenv("PASSWORD")
+
+DATABASE_URL = f"postgresql://{USER_NAME}:{PASSWORD}@{HOST}/{DB_NAME}"
+
 
 class DB:
     def __init__(self, database_url: str):
         self.engine = create_engine(database_url, echo=True)
-        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+        self.SessionLocal = sessionmaker(
+            autocommit=False, autoflush=False, bind=self.engine
+        )
         self.Base = declarative_base()
 
     def get_session(self):
@@ -16,6 +25,7 @@ class DB:
             yield db
         finally:
             db.close()
+
 
 # データベースのインスタンスを作成
 db = DB(DATABASE_URL)
