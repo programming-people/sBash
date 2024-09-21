26167006a7abbee65b91f63943f1f13d4d548cba
mewmer <unitia.uni@gmail.com>
2024-09-22 05:28:45 +0900

    fix style

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "../../public/styles.css";
+import "./globals.css";
 import { type ReactNode } from "react";
 import { type Metadata } from "next";
 import { DraftModeNotification } from "@/ui/components/DraftModeNotification";
--- frontend/src/ui/components/TabNavigation.tsx
+++ frontend/src/ui/components/TabNavigation.tsx
@@ -22,8 +22,8 @@ export function TabNavigation() {
 
 	return (
 		<>
-			<nav className="bg-gray-800">
-				<div className="mx-auto max-w-7xl px-6">
+			<nav className="shadow-md"> {/* {{ edit_1 }} 影を追加 */}
+				<div className="mx-auto max-w-7xl px-5">
 					<div className="flex h-12 items-center justify-start">
 						<div className="flex space-x-4">
 							{tabs.map((tab) => (
@@ -33,7 +33,7 @@ export function TabNavigation() {
 									className={`rounded-md px-3 py-2 text-sm font-medium ${
 										activeTab === tab
 											? "bg-gray-900 text-white"
-											: "text-gray-300 hover:bg-gray-700 hover:text-white"
+											: ""
 									}`}
 								>
 									{tab}
