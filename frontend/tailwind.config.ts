d5b58855541a99e5d55a10f39f8e03501ac25155
mewmer <unitia.uni@gmail.com>
2024-09-21 18:58:47 +0900

    fix: tailwind

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,11 +1,15 @@
-import TypographyPlugin from "@tailwindcss/typography";
-import FormPlugin from "@tailwindcss/forms";
-import ContainerQueriesPlugin from "@tailwindcss/container-queries";
 import { type Config } from "tailwindcss";
 
 const config: Config = {
-	content: ["./src/**/*.{ts,tsx}"],
-	plugins: [TypographyPlugin, FormPlugin, ContainerQueriesPlugin],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
 
 export default config;
