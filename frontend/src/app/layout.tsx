e3c912f1cb1c5f3b90096f3ede3d57c7ad6f4e78
mewmer <unitia.uni@gmail.com>
2024-09-22 01:20:40 +0900

    fix: tailwind build

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "./output.css";
 import { type ReactNode } from "react";
 import { type Metadata } from "next";
 import { DraftModeNotification } from "@/ui/components/DraftModeNotification";
