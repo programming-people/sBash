9c30068422d97205e7b4ead2d72b9ff9ba996b4d
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-09-22 11:13:21 +0900

    refactor

--- backend/load_env.py
+++ backend/load_env.py
@@ -12,3 +12,6 @@ DB_PASS = os.getenv("DB_PASS")
 DB_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 
 IMG_PATH = os.getenv("IMG_PATH")
+
+if not DB_HOST or not DB_USER or not DB_NAME or not DB_PASS or not IMG_PATH:
+    raise Exception("environment variables are required")
